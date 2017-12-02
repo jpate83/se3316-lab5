9bcf7c1761ab134cb9912952c74ac9df645f91ea
Jay Patel <jayp1924@gmail.com>
2017-12-02 05:00:40 +0000

    added nasa api service

--- client/src/app/nasa-api.service.ts
+++ client/src/app/nasa-api.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+const base = 'https://images-api.nasa.gov';
 
 @Injectable()
 export class NasaApiService {
-
-  constructor() { }
+    queryResults = {}; // cache
+    
+    constructor(private http: HttpClient) {}
+    
+    query(q, page, callback) {
+        if (this.queryResults[q] && this.queryResults[q][page]) {
+            callback(this.queryResults[q][page]);
+        } else {
+            let params = new HttpParams();
+            params.set('q', q);
+            params.set('media_type', 'image');
+            params.set('page', page);
+            this.http.get(base + '/search', { params }).subscribe(data => {
+                let resp = data["collection"];
+                if (resp)
+                    resp = resp["items"];
+                this.queryResults[q][page] = resp;
+                callback(resp);
+            }, err => {
+                console.log(err);
+                callback(undefined);
+            });
+        };
+    }
 
 }
